0d5154ef796af9d3adc76d58faa79f11ecf4b380
Louísa <github@louisa.uno>
2025-03-30 00:52:10 +0100

    Update registerRedirectClick.js

--- src/registerRedirectClick.js
+++ src/registerRedirectClick.js
@@ -1,6 +1,6 @@
 let lastClickTime = 0;
 
-function registerRedirectClick() {
+export function registerRedirectClick() {
     const currentTime = new Date().getTime();
     if (currentTime - lastClickTime < 500) {
         window.location.href = "https://go.louisa.uno/flame";
