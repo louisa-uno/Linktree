639130373149c82a7f044921b105286e8b33e5f6
Louísa <github@louisa.uno>
2025-03-30 01:06:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import './registerRedirectClick.js';
 import './updateHref.js';
 import './updatePronouns.js';
 import './codecorner.js';
-
-
-// Ensure the function is used
-document.querySelectorAll('.profilePictureContainer').forEach(el => {
-    el.addEventListener('click', registerRedirectClick);
-});
