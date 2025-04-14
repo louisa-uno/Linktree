4bc64971111c654fbea93c8fcd5ce3c898c96a75
Louísa <github@louisa.uno>
2025-04-14 20:21:30 +0200

    Added youtube link

---
+++ src/updateSubscribers.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import fileinput
+
+request = requests.request("GET", "https://vidiq.com/youtube-stats/channel/UC0xHqOSwS-kQioNw0NoeIFA/")
+
+soup = BeautifulSoup(request.text, 'html.parser')
+
+results = soup.find_all('p', class_='mb-0 text-right text-white')
+for result in results:
+	if 'K' in result.text:
+		subscriber_tag = result
+		break
+if subscriber_tag:
+    subscribers = subscriber_tag.text.strip()
+    print(subscribers)
+else:
+    raise("Subscriber count not found")
+
+with fileinput.FileInput("index.html", inplace=True) as file:
+    for line in file:
+        print(line.replace("YOUTUBE_SUBSCRIBERS", subscribers), end='')
