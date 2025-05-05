f4c0c7360a44493f94c870cba492284a07c89fb0
Louísa <github@louisa.uno>
2025-05-05 22:40:56 +0200

    Add script for setting up local assets

---
+++ setup-local-assets.sh
@@ -0,0 +1,8 @@
+curl -o fontawesome.css https://use.fontawesome.com/releases/v6.7.2/css/all.css
+mkdir webfonts
+curl -o webfonts/fa-brands-400.woff2 https://use.fontawesome.com/releases/v6.7.2/webfonts/fa-brands-400.woff2
+curl -o webfonts/fa-brands-400.ttf https://use.fontawesome.com/releases/v6.7.2/webfonts/fa-brands-400.ttf
+curl -o webfonts/fa-solid-900.woff2 https://use.fontawesome.com/releases/v6.7.2/webfonts/fa-solid-900.woff2
+curl -o webfonts/fa-solid-900.ttf https://use.fontawesome.com/releases/v6.7.2/webfonts/fa-solid-900.ttf
+curl -o SignikaGoogleFont.ttf https://fonts.gstatic.com/s/signika/v25/vEF72_JTCgwQ5ejvMV0Ox_Kg1UwJ0tKfX4zNpD8E4ASzH1r9XzyYzQ.ttf
+curl -o src/codecorner.js https://cdn.pride.codes/js/codecorner.js
