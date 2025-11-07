1479077283c7094e2def7f516beaf3f6bf173e07
Louísa <github@louisa.uno>
2025-11-07 11:29:36 +0100

    Refactor underline effect for pronouns: replace class manipulation with inline styles and remove unused CSS class

--- src/underlinePronouns.js
+++ src/underlinePronouns.js
@@ -2,10 +2,14 @@ const pronounElements = document.querySelectorAll(".pronouns");
 
 pronounElements.forEach((element) => {
 	element.addEventListener("mouseenter", () => {
-		pronounElements.forEach((el) => el.classList.add("underline-all"));
+		pronounElements.forEach((el) => {
+			el.style.textDecoration = "underline";
+		});
 	});
 
 	element.addEventListener("mouseleave", () => {
-		pronounElements.forEach((el) => el.classList.remove("underline-all"));
+		pronounElements.forEach((el) => {
+			el.style.textDecoration = "none";
+		});
 	});
 });
